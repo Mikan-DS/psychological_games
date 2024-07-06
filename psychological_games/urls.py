8d83857694c083cd67edcba741e625f524cc43c8
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-07-06 19:56:43 +0300

    vk bot

--- psychological_games/urls.py
+++ psychological_games/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = ([
     path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
+    path('vk_callback/', include('vkapi.urls'))
 ])
 
 if settings.DEBUG:
---
+++ vkapi/bot_config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# token issued on the VK group web-administration page
+token = ''
+# confirmation token issued on the VK group web-administration page in "Callback API" section
+confirmation_token = '9ea490cd'
+# secret key for cross site request forgery protection. It will be in each VK server request
+secret_key = 'DKMtmERJbsgT5HWp3NSLnYVaedcuF8qxk9C'
