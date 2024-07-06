4536166e11d2267e8424af95aaf89688293a53a1
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-07-06 19:01:29 +0300

    debug static

--- psychological_games/urls.py
+++ psychological_games/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = ([
     path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+])
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
