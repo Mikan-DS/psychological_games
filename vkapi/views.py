821295edf3df0a1da9c152a820ff6faf9aa043c6
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-07-06 20:49:35 +0300

    fix api

--- vkapi/views.py
+++ vkapi/views.py
@@ -1,11 +1,14 @@
 import json
 
 import vk
+import vk_api
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from vkapi.bot_config import *
 
+vk_session = vk_api.VkApi(token=token)
+api = vk_session.get_api()
 
 @csrf_exempt
 def index(request):
@@ -16,7 +19,7 @@ def index(request):
                 # confirmation_token from bot_config.py
                 return HttpResponse(confirmation_token, content_type="text/plain", status=200)
             if (data['type'] == 'message_new'):# if VK server send a message
-                api = vk.API(token, v=5.5)
+                # api = vk.API(token, v=5.5)
                 user_id = data['object']['user_id']
 
                 # token from bot_config.py
