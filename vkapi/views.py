6b1a8cfcedbd640b40193eef11263ab8cd170d52
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-07-12 18:41:29 +0300

    fix

--- vkapi/views.py
+++ vkapi/views.py
@@ -39,7 +39,9 @@ def index(request):
 
                 if data['object']['body'].startswith('!войти') or 'ref_source' in data['object']:
 
-                    if not User.objects.filter(username=user_id):
+                    user = User.objects.filter(username=user_id)
+
+                    if not user:
                         # api.messages.send(user_id=user_id, message="Пользователь не найден, создается новый.", random_id=0)
                         # User.objects.create_user(user_id, )
                         api.messages.send(
@@ -48,17 +50,29 @@ def index(request):
                             random_id=0)
 
                     else:
-                        expiry_duration = settings.confirmation_code_expiry if settings else timedelta(days=1)
-                        expires_at = datetime.now() + expiry_duration
-                        code = generate_random_code(4, string.digits)
-                        confirmation_code = ConfirmationCode(user_id=user_id, code=code, expires_at=expires_at)
-                        confirmation_code.save()
 
-                        api.messages.send(
-                            user_id=user_id,
-                            message=f"Ваш код для входа: {confirmation_code.code}\nТак-же вы можете зайти по ссылке"
-                                    f"{settings.host_url}/login/{confirmation_code.code}",
-                            random_id=0)
+                        try:
+                            user = user[0]
+
+                            expiry_duration = settings.confirmation_code_expiry if settings else timedelta(days=1)
+                            expires_at = datetime.now() + expiry_duration
+                            code = generate_random_code(4, string.digits)
+                            confirmation_code = ConfirmationCode(user=user, code=code, expires_at=expires_at)
+                            confirmation_code.save()
+
+                            api.messages.send(
+                                user_id=user_id,
+                                message=f"Ваш код для входа: {confirmation_code.code}\nТак-же вы можете зайти по ссылке"
+                                        f"{settings.host_url}/login/{confirmation_code.code}",
+                                random_id=0)
+                        except Exception as e:
+
+                            api.messages.send(
+                                user_id=user_id,
+                                message=f"Ошибка! {repr(e)}",
+                                random_id=0)
+
+
 
                 # token from bot_config.py
                 api.messages.send(user_id=user_id, message="Это тестовое сообщение для разработки, его не будет:\n"+str(data), random_id=0)
