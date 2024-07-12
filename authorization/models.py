020da5b415329973dfb9863a3e5c9c06d8abfdff
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-07-12 23:27:53 +0300

    fix

--- authorization/models.py
+++ authorization/models.py
@@ -1,24 +1,23 @@
-import string
-import random
+from datetime import timedelta
+
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime, timedelta
+from django.utils import timezone
 
-from authorization.utils import generate_random_code
 
 
 class ConfirmationCode(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=100)
     is_used = models.BooleanField(default=False)
-    expires_at = models.DateTimeField(default=datetime.now() + timedelta(days=1))
+    expires_at = models.DateTimeField(default=timezone.now() + timedelta(days=1))
 
     def __str__(self):
         return f"ConfirmationCode(user={self.user}, code={self.code}, is_used={self.is_used}, expires_at={self.expires_at})"
 
     @property
     def is_active(self):
-        return not self.is_used and datetime.now() < self.expires_at
+        return not self.is_used and timezone.now() < self.expires_at
 
 
 
