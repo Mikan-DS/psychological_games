10846bf8f289a2ee6a423c5a5867a5d0f115bf27
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-07-21 19:16:58 +0300

    Debug off

--- psychological_games/settings.py
+++ psychological_games/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n9ijlgx1jlvyuqjql5f!*&3z=^$4dowyjko*4v%p8*gxw%und('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "psygaonlin.pythonanywhere.com", "localhost"]
 
