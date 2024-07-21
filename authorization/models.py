9f1cda4b23c0274964494ba3bb899623b964da24
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-07-21 17:11:41 +0300

    Обновление админки

--- authorization/models.py
+++ authorization/models.py
@@ -18,23 +18,54 @@ class ConfirmationCode(models.Model):
     def is_active(self):
         return not self.is_used and timezone.now() < self.expires_at
 
+    class Meta:
+        verbose_name = "Код подтверждения"
+        verbose_name_plural = "Коды подтверждения"
+        ordering = ['-expires_at']
+
+
 
 class Settings(models.Model):
-    confirmation_code_expiry = models.DurationField(default=timedelta(hours=1))
-    vk_token = models.CharField(max_length=255, blank=True, null=True)
-    vk_confirmation_token = models.CharField(max_length=100, blank=True, null=True)
-    vk_secret_key = models.CharField(max_length=100, blank=True, null=True)
-    vk_chat_url = models.CharField(max_length=255, default="https://vk.com/im?sel=-199827634")
+    confirmation_code_expiry = models.DurationField(
+        default=timedelta(hours=1),
+        verbose_name="Срок действия кода подтверждения")
+    vk_token = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="VK токен")
+    vk_confirmation_token = models.CharField(max_length=100,
+                                             blank=True,
+                                             null=True,
+                                            verbose_name="VK токен подтверждения (Используется только при привязке)")
+    vk_secret_key = models.CharField(max_length=100,
+                                     blank=True,
+                                     null=True,
+                                     verbose_name="Секретный ключ для VK")
+    vk_chat_url = models.CharField(
+        max_length=255,
+        default="https://vk.com/im?sel=-199827634",
+        verbose_name="URL чата бота VK")
 
-    host_url = models.URLField(max_length=255, default="http://localhost:8000/")
-    pay_url = models.URLField(max_length=255, default="http://localhost:8000/")
+    host_url = models.URLField(
+        max_length=255,
+        default="http://localhost:8000/",
+        verbose_name="Ссылка сайта"
+    )
+    pay_url = models.URLField(
+        max_length=255,
+        default="http://localhost:8000/",
+        verbose_name="Ссылка сайта оплаты"
+    )
 
     send_debug_messages = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name = "Настройка"
+        verbose_name_plural = "Настройки"
+
     def __str__(self):
-        return (f"Settings(confirmation_code_expiry={self.confirmation_code_expiry}, "
-                f"vk_token={self.vk_token}, vk_confirmation_token={self.vk_confirmation_token}, "
-                f"vk_secret_key={self.vk_secret_key})")
+        return "Настройка"
 
     def save(self, *args, **kwargs):
         if not self.pk and Settings.objects.exists():
@@ -42,54 +73,69 @@ class Settings(models.Model):
         return super(Settings, self).save(*args, **kwargs)
 
 
-class OrderItem(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    item_type = models.CharField(max_length=255)
-
-    def save(self, *args, **kwargs):
-        if not self.item_type.isalnum() or ' ' in self.item_type or '.' in self.item_type:
-            raise ValueError("Тип товара может содержать только латиницу без пробелов и точек: " + self.item_type)
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
-
-
 class Purchase(models.Model):
-    id = models.AutoField(primary_key=True)
-    item_type = models.CharField(max_length=255) #TODO ИСПРАВИТЬ!!!
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    paid = models.BooleanField(default=False)
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    ITEM_TYPES = [
+        ('game', 'Игра'),
+        ('game_consultation', 'Игра и консультация'),
+    ]
+    item_type = models.CharField(max_length=255, choices=ITEM_TYPES, verbose_name="Тип")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость")
+    paid = models.BooleanField(default=False, verbose_name="Оплачено")
 
     def __str__(self):
-        return f"{self.item_type} by {self.user} for {self.cost} - {'Paid' if self.paid else 'Not Paid'}"
+        return f"{self.get_item_type_display()} от {self.user} за {self.cost} - {'Оплачено' if self.paid else 'Не оплачено'}"
+
+    class Meta:
+        verbose_name = "Покупка"
+        verbose_name_plural = "Покупки"
+
 
 
 class ContactWay(models.Model):
-    id = models.AutoField(primary_key=True)
-    method = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    method = models.CharField(max_length=255, verbose_name="Метод")
+    name = models.CharField(max_length=255, null=True, blank=True, verbose_name="Имя")
 
     def __str__(self):
-        return self.method
+        return self.name or self.method
+
+    class Meta:
+        verbose_name = "Способ контакта"
+        verbose_name_plural = "Способы контакта"
+
 
 
 class Age(models.Model):
-    id = models.AutoField(primary_key=True)
-    value = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    value = models.CharField(max_length=255, verbose_name="Возраст")
 
     def __str__(self):
         return self.value
 
+    class Meta:
+        verbose_name = "Возраст"
+        verbose_name_plural = "Возрасты"
+
+
 
 class ConsultationParameters(models.Model):
-    id = models.AutoField(primary_key=True)
-    question = models.TextField()
-    age = models.ForeignKey(Age, on_delete=models.CASCADE)
-    gender = models.BooleanField(default=True)  # True = 'М', False = 'Ж'
-    contact_way = models.ForeignKey(ContactWay, on_delete=models.CASCADE)
-    purchase = models.OneToOneField(Purchase, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    question = models.TextField(null=True, blank=True, verbose_name="Вопрос для консультации")
+    age = models.ForeignKey(Age, on_delete=models.CASCADE, verbose_name="Возраст")
+    GENDER_TYPES = [
+        (True, 'М'),
+        (False, 'Ж'),
+    ]
+    gender = models.BooleanField(default=True, choices=GENDER_TYPES, verbose_name="Пол")  # True = 'М', False = 'Ж'
+    contact_way = models.ForeignKey(ContactWay, on_delete=models.CASCADE, verbose_name="Способ связи")
+    purchase = models.OneToOneField(Purchase, on_delete=models.CASCADE, verbose_name="Покупка")
 
     def __str__(self):
-        return f"Consultation for {self.age}, {'М' if self.gender else 'Ж'}, via {self.contact_way}"
+        return f"Консультация для {self.age}, {'М' if self.gender else 'Ж'}, через {self.contact_way}"
+
+    class Meta:
+        verbose_name = "Параметры консультации"
+        verbose_name_plural = "Параметры консультаций"
+
