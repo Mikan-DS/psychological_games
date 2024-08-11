63193a3d3e3018aec99d32922ecb4c6e0fdf0323
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-08-11 08:29:39 +0300

    new update version

--- frontend/update-frontend-part.py
+++ frontend/update-frontend-part.py
@@ -6,21 +6,29 @@ if os.path.basename(os.getcwd()) != 'psychological_games_frontend':
     os.chdir('psychological_games_frontend')
 os.system('npm run build')
 
+with open("../template.html", "r", encoding="UTF-8") as file:
+    template = file.read()
+
 # Получаем список файлов в директории
 js_files = glob.glob('build/static/js/*.js')
 
+shutil.rmtree("../static/frontend/js/", ignore_errors=True)
 os.makedirs("../static/frontend/js/", exist_ok=True)
 # Копируем файлы в нужную директорию
-for file in js_files:
-    shutil.copy(file, '../static/frontend/js/psychological_games-app.js')
+js_file = [i for i in js_files if "main" in os.path.basename(i)][0]
+shutil.copy(js_file, f'../static/frontend/js/{os.path.basename(js_file)}')
 
 # Получаем список файлов в директории
 css_files = glob.glob('build/static/css/*.css')
 
+shutil.rmtree("../static/frontend/css/", ignore_errors=True)
 os.makedirs("../static/frontend/css/", exist_ok=True)
 # Копируем файлы в нужную директорию
-for file in css_files:
-    shutil.copy(file, '../static/frontend/css/psychological_games-app.css')
+css_file = [i for i in css_files if "main" in os.path.basename(i)][0]
+shutil.copy(js_file, f'../static/frontend/css/{os.path.basename(css_file)}')
 
 shutil.rmtree("../static/media", ignore_errors=True)
 shutil.copytree("build/static/media", "../static/media", dirs_exist_ok=True)
+
+with open("../templates/frontend/app.html", "w", encoding="UTF-8") as file:
+    file.write(template % (os.path.basename(js_file), os.path.basename(css_file)))
