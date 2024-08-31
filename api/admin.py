2684e3845e01ab4231b9c72575cd2192255a66e8
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-08-31 14:30:14 +0300

    cvs export

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,8 @@
+import csv
+
 from django.contrib import admin
 from django.forms import inlineformset_factory
+from django.http import HttpResponse
 from django.urls import reverse
 from django.utils.html import format_html
 
@@ -34,6 +37,26 @@ class ProjectSummaryAdmin(admin.ModelAdmin):
 admin.site.register(ProjectSummary, ProjectSummaryAdmin)
 
 
+def export_to_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="test_results.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Project', 'Name', 'IP', 'End Time', 'Duration'] +
+                    [param.name for param in ProjectParameter.objects.all()])
+
+    for obj in queryset:
+        row = [obj.project, obj.name, obj.ip, obj.end_time, obj.duration]
+        parameters = {param.name: param.value for param in obj.parameters.all()}
+        for param in ProjectParameter.objects.all():
+            row.append(parameters.get(param.name, ''))
+        writer.writerow(row)
+
+    return response
+
+
+export_to_csv.short_description = "Экспорт в CSV"
+
 class TestResultParameterInline(admin.TabularInline):
     model = TestResultParameter
     extra = 1
@@ -45,6 +68,8 @@ class TestResultAdmin(admin.ModelAdmin):
     list_display = ['project', 'name', 'ip', 'end_time', 'duration']
     search_fields = ['project__name', 'name', 'ip']
     list_filter = ['project', 'end_time']
+
+    actions = [export_to_csv]
 
 
 admin.site.register(TestResult, TestResultAdmin)
