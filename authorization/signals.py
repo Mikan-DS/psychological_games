33aa842e140ff7115be407cd4158af75d0d980f2
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-09-26 01:44:54 +0300

    PSY-2

--- authorization/signals.py
+++ authorization/signals.py
@@ -47,5 +47,5 @@ def create_default_settings(sender, **kwargs):
                     purchase.product = product
                     purchase.item_type = None
                     purchase.save()
-            except:
-                print("Purchase cannot be added: ", purchase.item_type)
+            except Exception as:
+                print("Purchase cannot be added: ", purchase.item_type, repr(e))
